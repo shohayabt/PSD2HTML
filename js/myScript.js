569ca7e3e57b913af3289f023707294ebd36e38b
Shohayab Hossain Talha <shohayabhasan@yahoo.com>
2021-09-25 16:05:45 +0600

    responsive complete

--- js/myScript.js
+++ js/myScript.js
@@ -25,8 +25,8 @@ $(function () {
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToShow: 1,
+              slidesToScroll: 1,
               infinite: true,
               dots: true
             }
@@ -61,12 +61,21 @@ $(function () {
         nextArrow: '<i class="fas fa-chevron-right next_arr"></i>',
         responsive: [
           {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
             breakpoint: 768,
             settings: {
               arrows: false,
               centerMode: true,
               centerPadding: '40px',
-              slidesToShow: 3
+              slidesToShow: 2
             }
           },
           {
@@ -96,14 +105,15 @@ $(function () {
               slidesToShow: 3,
               slidesToScroll: 3,
               infinite: true,
-              dots: true
+              dots: false
             }
           },
           {
             breakpoint: 600,
             settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              dots: false
             }
           },
           {
